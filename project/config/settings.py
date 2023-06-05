934f820e7f82ebccb322b52702d68d3868e68c2e
Luca Cosatto <lucacosatto@hotmail.com>
2023-06-05 13:35:18 -0300

    create models en home, post y autor

--- project/config/settings.py
+++ project/config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import sys
-APLICACIONES= BASE_DIR /"apps"
+APLICACIONES= BASE_DIR / "apps"
 sys.path.append(str(APLICACIONES))
 
 # Quick-start development settings - unsuitable for production
